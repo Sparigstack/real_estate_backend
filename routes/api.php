3cfab52108cb6360a7f7ae4a4c95f73201c4aaf7
tithisprigstack <tithi.sprigstack@outlook.com>
2024-09-19 11:14:22 +0530

    Unit detail and wingdetail call changes

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\UserController;
 Route::post('/register-user', [AuthController::class, 'registerUser']);
 Route::post('/check-user-otp', [AuthController::class, 'checkUserOtp']);
 
-// Route::middleware('auth:api')->group(function (){
+Route::middleware('auth:api')->group(function (){
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user-profile/{uid}', [UserController::class, 'userProfile']);
     Route::post('/add-update-user-profile', [UserController::class, 'addUpdateUserProfile']);
@@ -17,5 +17,6 @@ Route::post('/check-user-otp', [AuthController::class, 'checkUserOtp']);
     Route::get('/get-property-statuses/{statusFlag}', [PropertyController::class, 'getPropertyStatues']);
     Route::get('/get-property-amenities', [PropertyController::class, 'getPropertyAmenities']);
     Route::post('/add-wing-details', [PropertyController::class, 'addWingDetails']);
+    Route::post('/add-unit-details', [PropertyController::class, 'addUnitDetails']);
 
-// });
+});
